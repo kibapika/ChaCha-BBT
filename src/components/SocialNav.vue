0c7980db5e52ab80c6d6372a52252a01afd9132f
Eva Li <evali3101@gmail.com>
2024-05-21 20:42:35 -0400

    edited homepg; added video

--- src/components/SocialNav.vue
+++ src/components/SocialNav.vue
@@ -2,10 +2,21 @@
 
 <template>
   <div>
-    <v-icon name="bi-facebook" />
-    <v-icon name="bi-twitter" />
-    <v-icon name="bi-instagram" />
-    <v-icon name="bi-tiktok" />
-    <v-icon name="hi-mail" />
+    <v-icon class="icon" hover scale="1.25" animation="float" name="bi-facebook" />
+    <v-icon class="icon" hover scale="1.25" animation="float" name="bi-twitter" />
+    <v-icon class="icon" hover scale="1.25" animation="float" name="bi-instagram" />
+    <v-icon class="icon" hover scale="1.25" animation="float" name="bi-tiktok" />
+    <v-icon class="icon" hover scale="1.25" animation="float" name="hi-mail" />
   </div>
 </template>
+
+<style scoped>
+.icon {
+  cursor: pointer;
+  color: white;
+}
+
+.icon:hover {
+  color: aqua;
+}
+</style>
--- src/views/HomePg.vue
+++ src/views/HomePg.vue
@@ -1,7 +1,11 @@
 <script setup>
-
+// import video form '../assets/teaVid.mp4';
+import video from '../assets/teaVid.mp4'
 </script>
 
 <template>
-    <h1>Home</h1>
+  <h1>Home</h1>
+  <video width="320" height="240" controls>
+    <source :src="video" type="video/mp4">
+  </video>
 </template>
