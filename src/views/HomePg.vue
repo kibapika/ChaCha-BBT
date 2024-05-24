bab3bb8c78c995267a7a250405b63d3ef06d30d4
Eva Li <evali3101@gmail.com>
2024-05-24 15:30:27 -0400

    eidted home pg, video and title

--- src/views/HomePg.vue
+++ src/views/HomePg.vue
@@ -4,8 +4,33 @@ import video from '../assets/teaVid.mp4'
 </script>
 
 <template>
-  <h1>Home</h1>
-  <video width="320" height="240" controls>
-    <source :src="video" type="video/mp4">
-  </video>
+  <div class="homepage-video">
+    <video width="100%" muted loop autoplay>
+      <source :src="video" type="video/mp4" />
+    </video>
+    <div class="text-over-video">
+      <h1>Welcome to ChaCha</h1>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.homepage-video,
+.homepage-video video {
+  position: relative;
+}
+.homepage-video video {
+  background-size: cover !important;
+  background-repeat: no-repeat !important;
+  background-position: 50% !important;
+}
+
+.text-over-video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: black;
+}
+</style>
