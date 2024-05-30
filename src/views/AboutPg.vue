d9ab403abfb23e4ed582a997343237dd5e950fb6
Eva Li <evali3101@gmail.com>
2024-05-30 17:00:47 -0400

    edited aboutpg css

--- src/views/AboutPg.vue
+++ src/views/AboutPg.vue
@@ -4,42 +4,63 @@ import tea from '../assets/tea.jpg'
 
 <template>
   <div id="aboutdiv">
-    <img id="teapic" width="40%" alt="teaaboutpg" :src="tea" />
-    <section id="aboutsec">
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, eius officia. Eaque
-        similique qui ducimus, iusto est nulla, beatae a quidem ad facilis dolores repudiandae hic
-        optio modi id quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore hic
-        soluta corrupti, veritatis ut alias sint dolorem voluptates enim, excepturi est eos laborum
-        omnis voluptatem, ratione magni facilis sit exercitationem!<br />Lorem ipsum, dolor sit amet
-        consectetur adipisicing elit. Fuga aut cumque repellendus, neque maiores laborum accusamus
-        in temporibus atque minus sint et incidunt harum ex omnis numquam impedit voluptatibus
-        sapiente. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore molestias sunt
-        exercitationem dolore eaque tenetur labore maiores, illum ut quod impedit quas est, facilis
-        minus dignissimos recusandae amet iusto? Odio. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Quos quis nesciunt, repellat mollitia sunt repudiandae expedita in atque
-        molestias ex, dolorum iste explicabo suscipit ducimus vel non, at modi soluta.
-      </p>
-    </section>
+    <div id="container">
+      <img id="teapic" alt="teaaboutpg" :src="tea" />
+      <section class="box2">
+        <h1 class="text title">The Story of ChaCha</h1>
+        <p class="text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, eius officia. Eaque
+          similique qui ducimus, iusto est nulla, beatae a quidem ad facilis dolores repudiandae hic
+          optio modi id quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Fuga aut cumque repellendus, neque
+          maiores laborum accusamus in temporibus atque minus sint et incidunt harum ex omnis
+          numquam impedit voluptatibus sapiente. elit.
+        </p>
+      </section>
+    </div>
   </div>
 </template>
 
 <style scoped>
 #teapic {
-  border-top-left-radius: 20%;
-  border-top-right-radius: 20%;
+  max-width: 28dvw;
+  width: 28dvw;
+  border-radius: 100%;
+  box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
+  position: absolute;
+  top: 20px;
+  left: 125px;
 }
 
-#aboutdiv{
-    display: flex;
-    flex-direction: row;
-    place-items: center;
-    justify-content: space-around;
-
+#aboutdiv {
+  height: 100dvh;
+  display: flex;
+  justify-content: center;
 }
 
-#aboutsec{
-    width: 50%;
+#container {
+  margin-top: 2.25rem;
+  margin-bottom: 2.25rem;
+  width: 85%;
+  position: relative;
+}
+
+.box2 {
+  padding: 1rem;
+  border-radius: 0.75rem;
+  box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
+  width: 35dvw;
+  max-width: 35dvw;
+  color: black;
+  top: 250px;
+  right: 140px;
+  position: absolute;
+  background: rgba(255, 255, 255, 1);
+}
+
+.title {
+    font-weight: bolder;
+    line-height: 2;
+    font-size: 23px;
 }
 </style>
