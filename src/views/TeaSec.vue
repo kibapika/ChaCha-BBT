16b8c095f99b4417c53161708795126f34a372b1
Eva Li <evali3101@gmail.com>
2024-05-31 17:21:55 -0400

    editing teasec

--- src/views/TeaSec.vue
+++ src/views/TeaSec.vue
@@ -1,21 +1,24 @@
 <script setup>
 import specialTea from '../assets/specialTea.jpg'
+import specialTea2 from '../assets/specialTea2.jpg'
+import fruitTea from '../assets/fruitTea.jpg'
+import milkTea from '../assets/milkTea.jpg'
 </script>
 
 <template>
-  <div>
+  <div id="teaSecdiv">
     <h1 id="milkTeaTitle">Delicious Tea</h1>
     <div id="milkSec">
       <section class="milkTeaBtn">
-        <img width="50%" alt="specialTeaPic" :src="specialTea" />
+        <img width="150px" alt="specialTeaPic" :src="milkTea" />
         <router-link to="/menu"><h1>Milk Teas</h1></router-link>
       </section>
       <section class="milkTeaBtn">
-        <img width="50%" alt="specialTeaPic" :src="specialTea" />
+        <img width="150px" alt="specialTeaPic" :src="fruitTea" />
         <router-link to="/menu"><h1>Fruite Teas</h1></router-link>
       </section>
       <section class="milkTeaBtn">
-        <img width="50%" alt="specialTeaPic" :src="specialTea" />
+        <img width="150px" alt="specialTeaPic" :src="specialTea2" />
         <router-link to="/menu"><h1>SpecialTeas</h1></router-link>
       </section>
     </div>
@@ -23,6 +26,13 @@ import specialTea from '../assets/specialTea.jpg'
 </template>
 
 <style scoped>
+#teaSecdiv{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 80dvh;
+}
+
 #milkTeaTitle {
   text-align: center;
 }
@@ -31,6 +41,7 @@ import specialTea from '../assets/specialTea.jpg'
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  align-items: center;
 }
 
 .milkTeaBtn {
