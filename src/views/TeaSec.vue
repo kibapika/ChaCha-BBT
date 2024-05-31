df9881e7a0e29c980eb7a38c72a3feebf2969db9
Eva Li <evali3101@gmail.com>
2024-05-31 15:27:01 -0400

    editing tesec

--- src/views/TeaSec.vue
+++ src/views/TeaSec.vue
@@ -1,8 +1,32 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h1>Delicious Tea</h1>
-    <div></div>
+  <div id="teaSec">
+    <h1 id="milkTeaTitle">Delicious Tea</h1>
+    <div id="milkSec">
+      <section>
+        <router-link to="/menu"><h1>Milk Teas</h1></router-link>
+      </section>
+      <section>
+        <router-link to="/menu"><h1>Fruite Teas</h1></router-link>
+      </section>
+      <section>
+        <router-link to="/menu"><h1>Specialty Teas</h1></router-link>
+      </section>
+    </div>
   </div>
 </template>
+
+<style scoped>
+#milkTeaTitle {
+  text-align: center;
+}
+
+#milkSec {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+}
+</style>
