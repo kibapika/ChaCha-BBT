99cc032ba13cd3009f07b8579704f1b50f40741c
Eva Li <evali3101@gmail.com>
2024-07-11 15:07:26 -0400

    editing css of menupg' '

--- src/components/BubbleMenu/FruitTeas.vue
+++ src/components/BubbleMenu/FruitTeas.vue
@@ -9,7 +9,7 @@ export default {
     <ul>
         <li>Mango Green Tea - Green Tea, Mango Flavor, Mango Jelly</li>
         <li>Passion Fruit Black Tea - Black Tea, Passion Fruit, Popping Boba</li>
-        <li>Strawberry Lemonade Tea - Black Tea, Strawberry Flavor, Lemon Slices, Strawberry Slices</li>
+        <li>Strawberry Lemonade Tea - Black Tea, Strawberry Flavor, Lemon + Strawberry Slices</li>
         <li>Peach Oolong Tea - Oolong Tea, Peach Flavor, Lychee Jelly</li>
         <li>Lychee Green Tea - Green Tea, Lychee Flavor, Fresh Lychee</li>
       </ul>
--- src/views/MenuPg.vue
+++ src/views/MenuPg.vue
@@ -28,66 +28,117 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="tabs" role="tablist">
-      <button
-        v-for="(tab, index) in tabs"
-        :key="index"
-        @click="selectTab(index)"
-        :class="{ active: activeTab === index }"
-        role="tab"
-        :aria-selected="activeTab === index"
-        :aria-controls="'tab-' + index"
-        :id="'tab-btn-' + index"
-      >
-        {{ tab.name }}
-      </button>
-    </div>
-    <div class="tab-content">
-      <component :is="tabs[activeTab].component"></component>
-    </div>
+  <h1 class="menuTitle">Menu</h1>
+  <div class="menuDiv">
     <section>
-      <h1>Toppings</h1>
-      <ul>
-        <li>Tapioca Peals</li>
-        <li>Popping Boba (various flavor)</li>
-        <li>Aloe Vera</li>
-        <li>Pudding</li>
-        <li>Grass Jelly</li>
-        <li>Lychee Jelly</li>
-        <li>Mango Jelly</li>
-        <li>Coconut Jelly</li>
-        <li>Red Bean</li>
-      </ul>
+      <div class="tabs" role="tablist">
+        <button
+          v-for="(tab, index) in tabs"
+          :key="index"
+          @click="selectTab(index)"
+          :class="{ active: activeTab === index }"
+          role="tab"
+          :aria-selected="activeTab === index"
+          :aria-controls="'tab-' + index"
+          :id="'tab-btn-' + index"
+        >
+          {{ tab.name }}
+        </button>
+      </div>
+      <div class="tab-content">
+        <component :is="tabs[activeTab].component"></component>
+      </div>
     </section>
-    <section>
-      <h1>Customizaion Options</h1>
-      <ul>
-        <li>Sugar Levels: 0%, 25%, 50%, 75%, 100%</li>
-        <li>Ice Levels: No Ice, Less Ice, Regular Ice, Extra Ice</li>
-        <li>Milk Alternatives: Whole Milk, Skim Milk, Almond Milk, Oat Milk</li>
-      </ul>
-    </section>
+    <div class="menuCustom">
+      <section>
+        <h1>Toppings</h1>
+        <ul>
+          <li>Tapioca Peals</li>
+          <li>Popping Boba (various flavor)</li>
+          <li>Aloe Vera</li>
+          <li>Pudding</li>
+          <li>Grass Jelly</li>
+          <li>Lychee Jelly</li>
+          <li>Mango Jelly</li>
+          <li>Coconut Jelly</li>
+          <li>Red Bean</li>
+        </ul>
+      </section>
+      <section>
+        <h1>Customizaion Options</h1>
+        <ul>
+          <li>Sugar Levels: 0%, 25%, 50%, 75%, 100%</li>
+          <li>Ice Levels: No Ice, Less Ice, Regular Ice, Extra Ice</li>
+          <li>Milk Alternatives: Whole Milk, Skim Milk, Almond Milk, Oat Milk</li>
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
 
 <style>
+.menuTitle {
+    background-color: #baa898;
+    height: 18dvh;
+    font-size: 55px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+.menuDiv {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+}
+
 .tabs {
   display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 button {
+  width: 250px;
+  height: 50px;
+  font-size: 20px;
   padding: 10px;
   margin-right: 5px;
   cursor: pointer;
+  border: none;
+  box-shadow:
+    0 4px 6px -1px rgb(0 0 0 / 0.1),
+    0 2px 4px -2px rgb(0 0 0 / 0.1);
+  background-color: white;
+  border-radius: 15px;
 }
 
 button.active {
-  background-color: #42b983;
-  color: white;
+  border: none;
+  background-color: #bcbd8b;
+  color: #eb5e28;
 }
 
 .tab-content {
   margin-top: 20px;
+  height: 300px;
+  border-radius: 25px;
+  box-shadow:
+    0 4px 6px -1px rgb(0 0 0 / 0.1),
+    0 2px 4px -2px rgb(0 0 0 / 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+}
+
+.menuCustom {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 80%;
+  padding-top: 10px;
 }
 </style>
