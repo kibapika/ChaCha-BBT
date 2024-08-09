aa6b649b81eaad5246e243a69ac59cd7d06e3ef0
Eva Li <evali3101@gmail.com>
2024-08-09 15:51:08 -0400

    started on location, created API, installed vue3 google maps

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,24 @@
+<script>
+import { defineComponent } from 'vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 }
+
+    return { center }
+  }
+})
+</script>
+
+<template>
+  <GoogleMap
+    api-key="AIzaSyDZlnFNPmm6Ear2-Iot0wqb6htn3s_bZ7c"
+    style="width: 50%; height: 500px"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
--- src/views/LocationPg.vue
+++ src/views/LocationPg.vue
@@ -1,7 +1,31 @@
 <script setup>
-
+import GoogleMap from '../components/GoogleMap.vue'
 </script>
 
 <template>
-    <h1>Locations</h1>
+  <section class="titleBox">
+    <h1 class="menuTitle">Locations</h1>
+  </section>
+  <section>
+    <GoogleMap />
+  </section>
 </template>
+
+<style scoped>
+.titleBox {
+  background-image: url('../assets/driedTea.jpg');
+  background-size: cover;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+.menuTitle {
+  width: 80%;
+  height: 18dvh;
+  font-size: 55px;
+  display: flex;
+  align-items: center;
+  color: white;
+}
+</style>
