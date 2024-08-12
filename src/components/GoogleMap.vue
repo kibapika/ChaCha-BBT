df972f955bb150ba3640de52970a7b5281f9c6ac
Eva Li <evali3101@gmail.com>
2024-08-12 12:07:56 -0400

    stores location works

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -5,12 +5,12 @@ import { GoogleMap, Marker } from 'vue3-google-map'
 export default defineComponent({
   components: { GoogleMap, Marker },
   setup() {
-    const center = { lat: 40.689247, lng: -74.044502 }
+    const center = { lat: 40.7128, lng: -74.0060 }
     const stores = [
-    { position: { lat: 40.73061, lng: -73.935242 }, name: 'Store 1' },
-    { position: { lat: 40.712776, lng: -74.005974 }, name: 'Store 2' },
-    { position: { lat: 40.758896, lng: -73.985130 }, name: 'Store 3' },
-  ]
+      { position: { lat: 40.73061, lng: -73.935242 }, name: 'Store 1' },
+      { position: { lat: 40.712776, lng: -74.005974 }, name: 'Store 2' },
+      { position: { lat: 40.758896, lng: -73.98513 }, name: 'Store 3' }
+    ]
 
     return { center, stores }
   }
@@ -24,6 +24,6 @@ export default defineComponent({
     :center="center"
     :zoom="15"
   >
-    <Marker :options="{ position: stores.position }" />
+    <Marker v-for="(store, index) in stores" :key="index" :options="{ position: store.position }" />
   </GoogleMap>
 </template>
