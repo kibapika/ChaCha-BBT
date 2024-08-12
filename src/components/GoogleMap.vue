53f55f2cfebb8bd3c9bc9dc81644d172a9676031
Eva Li <evali3101@gmail.com>
2024-08-12 12:03:19 -0400

    trying out different stores on map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -6,8 +6,13 @@ export default defineComponent({
   components: { GoogleMap, Marker },
   setup() {
     const center = { lat: 40.689247, lng: -74.044502 }
+    const stores = [
+    { position: { lat: 40.73061, lng: -73.935242 }, name: 'Store 1' },
+    { position: { lat: 40.712776, lng: -74.005974 }, name: 'Store 2' },
+    { position: { lat: 40.758896, lng: -73.985130 }, name: 'Store 3' },
+  ]
 
-    return { center }
+    return { center, stores }
   }
 })
 </script>
@@ -19,6 +24,6 @@ export default defineComponent({
     :center="center"
     :zoom="15"
   >
-    <Marker :options="{ position: center }" />
+    <Marker :options="{ position: stores.position }" />
   </GoogleMap>
 </template>
