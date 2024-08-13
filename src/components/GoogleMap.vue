ce1e1ef7dabb1820ec4256266e3e4e8bb9face9e
Eva Li <evali3101@gmail.com>
2024-08-13 15:53:06 -0400

    info window not working in googlemap

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,18 +1,57 @@
 <script>
-import { defineComponent } from 'vue'
-import { GoogleMap, Marker } from 'vue3-google-map'
+import { defineComponent, ref } from 'vue'
+import { GoogleMap, InfoWindow, Marker } from 'vue3-google-map'
 
 export default defineComponent({
-  components: { GoogleMap, Marker },
+  components: { GoogleMap, Marker, InfoWindow },
   setup() {
-    const center = { lat: 40.7128, lng: -74.0060 }
+    const center = { lat: 40.7128, lng: -74.006 }
     const stores = [
-      { position: { lat: 40.73061, lng: -73.935242 }, name: 'Store 1' },
-      { position: { lat: 40.712776, lng: -74.005974 }, name: 'Store 2' },
-      { position: { lat: 40.758896, lng: -73.98513 }, name: 'Store 3' }
+      {
+        position: { lat: 40.73061, lng: -73.935242 },
+        name: 'Store 1',
+        description: 'This is Store 1 located in New York City.',
+        label: {
+          text: 'S1',
+          fontSize: '14px',
+          fontWeight: 'bold'
+        }
+      },
+      {
+        position: { lat: 40.712776, lng: -74.005974 },
+        name: 'Store 2',
+        description: 'This is Store 2 located in New York City.',
+        label: {
+          text: 'S2',
+          fontSize: '14px',
+          fontWeight: 'bold'
+        }
+      },
+      {
+        position: { lat: 40.758896, lng: -73.98513 },
+        name: 'Store 3',
+        description: 'This is Store 3 located in New York City.',
+        label: {
+          text: 'S3',
+          fontSize: '14px',
+          fontWeight: 'bold'
+        }
+      }
     ]
 
-    return { center, stores }
+    const selectedStore = ref(null)
+
+    // const openInfoWindow = (store) => {
+    //   console.log('clicked')
+    //   selectedStore.value = store
+    // }
+
+    const openInfoWindow = (store) => {
+      alert(JSON.stringify(store)) // Check if correct store data is passed
+      selectedStore.value = store
+    }
+
+    return { center, stores, selectedStore, openInfoWindow }
   }
 })
 </script>
@@ -22,8 +61,20 @@ export default defineComponent({
     api-key="AIzaSyDZlnFNPmm6Ear2-Iot0wqb6htn3s_bZ7c"
     style="width: 50%; height: 500px"
     :center="center"
-    :zoom="15"
+    :zoom="13"
   >
-    <Marker v-for="(store, index) in stores" :key="index" :options="{ position: store.position }" />
+    <Marker
+      v-for="(store, index) in stores"
+      :key="index"
+      :options="{ position: store.position, label: store.label }"
+      @click="openInfoWindow(store)"
+    />
+
+    <InfoWindow :position="{ lat: 40.7128, lng: -74.006 }" @closeclick="selectedStore = null">
+      <div>
+        <h3>Static Store</h3>
+        <p>This is a static position test.</p>
+      </div>
+    </InfoWindow>
   </GoogleMap>
 </template>
